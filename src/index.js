a511e74919742fc973df1bfa4f0f339c2306c730
K Adam White <kadamwhite@users.noreply.github.com>
2020-10-29 13:51:49 -0400

    Remove anything we don't need

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Load all SVGs from svgs/ folder using require.context
+// Docs: https://webpack.js.org/guides/dependency-management/#requirecontext
+const requireImg = require.context( './svgs', false, /\.svg$/ );
+// requireImg is now both a "context", meaning it has a list of matched modules,
+// but also a function that behaves like `require()` does (similar to `import`).
+// To get an array of the icon paths, we pass each module in the context to
+// the module function. It's opaque but that's Webpack for you... not ideal.
+const imgSources = requireImg.keys().map( module => requireImg( module ).default );
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      { imgSources.map( imgPath => (
+        <img src={ imgPath } alt="this is descriptive I promise" />
+      ) ) }
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
